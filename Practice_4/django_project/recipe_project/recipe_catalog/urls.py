f4709451a4e02a5b5341cd5b2aaaa6fe1ba0589d
breez <ilya66401@gmail.com>
2024-11-17 11:24:49 +0300

    upd: small changes in Practice_4

--- Practice_4/django_project/recipe_project/recipe_catalog/urls.py
+++ Practice_4/django_project/recipe_project/recipe_catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import about, index, recipe_detail
 
+app_name = 'recipe_catalog'
+
 urlpatterns = [
 	path('', index, name='index'),
 	path('about/', about, name='about'),
